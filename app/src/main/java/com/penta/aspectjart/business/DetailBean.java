0888ff21bdb2d2b152a24e115069a75eabd019e4
linyueyang <linyueyang@58.com>
2018-11-09 15:44:38 +0800

    demo optimization

---
+++ app/src/main/java/com/penta/aspectjart/business/DetailBean.java
@@ -0,0 +1,45 @@
+package com.penta.aspectjart.business;
+
+/**
+ * Created by linyueyang on 2018/11/8.
+ */
+
+public class DetailBean {
+
+    String id;
+    String pagetype;
+    String title;
+    String content;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPagetype() {
+        return pagetype;
+    }
+
+    public void setPagetype(String pagetype) {
+        this.pagetype = pagetype;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
